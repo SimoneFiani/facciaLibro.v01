4bb3e08ee53972d1cbdfccad583fa4b647c0c0b1
Simone <Simone@DESKTOP-1HCPJ4L.station>
2020-09-23 23:07:40 +0200

    risolto problema su index/form-utenti

--- src/main/java/it/fiani/facciaLibro/service/UtenteService.java
+++ src/main/java/it/fiani/facciaLibro/service/UtenteService.java
@@ -21,9 +21,10 @@ public class UtenteService {
 	}
 
 	public void salva(Utente utente) {
-		System.out.println(utente);
+
 		utente.setInstanteCreazione(LocalDateTime.now());
 		utente.setInstanteUltimoAggiornamento(LocalDateTime.now());
+		System.out.println(utente);
 		utenteRepository.save(utente);
 
 	}
